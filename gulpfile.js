ee0c95f81156ebbf62b4416c4f9ef15d80c58a31
Michael <mraypold@users.noreply.github.com>
2015-09-10 08:17:48 -0600

    integrated gulp with express

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
+var server = require('gulp-express');
 
 var src = {
   bower: 'bower_components/',
@@ -25,12 +26,24 @@ gulp.task('copy', function() {
     .pipe(gulp.dest(dst.css));
   // copy javascript
   gulp.src([
-    src.bower + src.jqueryfp + src.jquery
-  ])
-  .pipe(gulp.dest(dst.js))
+      src.bower + src.jqueryfp + src.jquery
+    ])
+    .pipe(gulp.dest(dst.js))
 });
 
 gulp.task('clean', function() {
   del([dst.css + src.skeleton, dst.css + src.normalize]);
   del(dst.js + src.jquery);
+});
+
+gulp.task('server', function() {
+  server.run(['app.js']);
+
+  // Watch for changes
+  gulp.watch([dst.css + '*.css',
+    dst.img + '*.png',
+    dst.js + '.js'
+  ], server.notify);
+
+  gulp.watch(['app.js'], [server.run]);
 });
